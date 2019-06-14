d9bd50404e3679e3c2b20864f778d71d4672d973
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-06-14 15:42:21 -0400

    Minor cleanup, fix issue with refactored class having same name as other function

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,6 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMaker {
+class CuboidMakerRefactored {
     constructor(props) {
         this.length = props.length;
         this.width = props.width;
@@ -16,7 +16,7 @@ class CuboidMaker {
     }
 }
 
-var cuboid = new CuboidMaker({
+var cuboid = new CuboidMakerRefactored({
     length: 4,
     width: 5,
     height: 5
@@ -28,7 +28,7 @@ console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
-class CubeMaker extends CuboidMaker {
+class CubeMaker extends CuboidMakerRefactored {
     constructor(props) {
         super(props);
     }
